cbd4f482d2472cbdc2383ad449e8e7fa5da36ad4
Faisal Hassan Ahmed Sumayli <152008390+xntsO@users.noreply.github.com>
2026-08-19 11:12:00 +0300

    test(appium): parameterize channel identity

--- appium-tests/tests/test_vendroid_smoke.py
+++ appium-tests/tests/test_vendroid_smoke.py
@@ -1,3 +1,5 @@
+import os
+
 import appium.webdriver
 
 from vendroid.fixtures import appium_service, driver
@@ -7,5 +9,7 @@ used(appium_service)
 
 
 def test_home_shows_install_ventoy_action(driver: appium.webdriver.Remote):
+    app_name = os.environ.get("VENDROID_APP_NAME", "Vendroid")
+    version = os.environ.get("VENDROID_EXPECTED_VERSION", "0.2.0")
     wait_for_element(driver, '//*[@resource-id="installVentoyCTA"]', timeout=15)
-    wait_for_element(driver, '//*[@text="Vendroid v0.1"]', timeout=15)
+    wait_for_element(driver, f'//*[@text="{app_name} v{version}"]', timeout=15)
